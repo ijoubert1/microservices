b096cff955699c62ca913f5237b72e130c8f72dc
Irian Carolina Joubert Castellanos <ijoubert@technisys.com>
2021-05-15 17:38:06 -0300

    Refactoring user-service

---
+++ spring-boot-service-users/src/main/java/com/formacionbdi/spring/app/users/models/dao/UserDao.java
@@ -0,0 +1,22 @@
+package com.formacionbdi.spring.app.users.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.formacionbdi.spring.app.users.commons.models.entity.User;
+
+@RepositoryRestResource(path="users")
+public interface UserDao extends PagingAndSortingRepository<User, Long> {
+	
+	@RestResource(path="find-username")
+	public User findByUsername(@Param("username") String username);
+	
+	public User findByUsernameAndEmail(@Param("username") String username, @Param("email")  String email);
+	
+	@Query("select u from User u where u.username=?1")
+	public User customQuery(String username);
+
+}
---
+++ spring-boot-service-users/src/test/java/com/formacionbdi/spring/app/users/SpringBootServiceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.spring.app.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
