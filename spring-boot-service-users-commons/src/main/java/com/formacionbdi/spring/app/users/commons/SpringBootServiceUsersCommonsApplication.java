7067c943a7f407e7593de4a86cd2683a58b8bd9d
Irian Carolina Joubert Castellanos <ijoubert@technisys.com>
2021-05-15 16:57:00 -0300

    User-common microservice

---
+++ spring-boot-service-users-commons/src/main/java/com/formacionbdi/spring/app/users/commons/SpringBootServiceUsersCommonsApplication.java
@@ -0,0 +1,11 @@
+package com.formacionbdi.spring.app.users.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class SpringBootServiceUsersCommonsApplication {
+
+}
---
+++ spring-boot-service-users-commons/src/test/java/com/formacionbdi/spring/app/users/commons/SpringBootServiceUsersCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.spring.app.users.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceUsersCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
