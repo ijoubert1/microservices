624a1a07ec7452af499de9ffcb40f1ddd02757bf
Irian Carolina Joubert Castellanos <ijoubert@technisys.com>
2021-05-17 12:11:27 -0300

    Fixing pom version in order to connect to config server and mysql

--- spring-boot-service-products/src/test/java/com/formacionbdi/spring/app/products/SpringBootServiceProductsApplicationTests.java
+++ spring-boot-service-products/src/test/java/com/formacionbdi/spring/app/products/SpringBootServiceProductsApplicationTests.java
@@ -1,6 +1,6 @@
 package com.formacionbdi.spring.app.products;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
