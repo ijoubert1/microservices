e9524cc69951549522f44d167cd3f334cc1796a2
Irian Carolina Joubert Castellanos <ijoubert@technisys.com>
2021-05-17 13:28:03 -0300

    Fixing user-service with common lib - Changing pom version

--- spring-boot-service-users-commons/src/test/java/com/formacionbdi/spring/app/users/commons/SpringBootServiceUsersCommonsApplicationTests.java
+++ spring-boot-service-users-commons/src/test/java/com/formacionbdi/spring/app/users/commons/SpringBootServiceUsersCommonsApplicationTests.java
@@ -1,6 +1,6 @@
 package com.formacionbdi.spring.app.users.commons;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- spring-boot-service-users/src/main/java/com/formacionbdi/spring/app/users/SpringBootServiceUsersApplication.java
+++ spring-boot-service-users/src/main/java/com/formacionbdi/spring/app/users/SpringBootServiceUsersApplication.java
@@ -4,11 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.formacionbdi.spring.app.users.commons.SpringBootServiceUsersCommonsApplication;
+import com.formacionbdi.spring.app.users.commons.models.entity.Role;
+import com.formacionbdi.spring.app.users.commons.models.entity.User;
 
-@EntityScan({"com.formacionbdi.springboot.app.users.commons.models.entity"})
+//@EntityScan({"com.formacionbdi.springboot.app.users.commons.models.entity"})
+@ComponentScan
 @SpringBootApplication
 public class SpringBootServiceUsersApplication {
 	private static Logger log = LoggerFactory.getLogger(SpringBootServiceUsersApplication.class);
@@ -16,6 +19,15 @@ public class SpringBootServiceUsersApplication {
 	public static void main(String[] args) {
 		SpringBootServiceUsersCommonsApplication common = new SpringBootServiceUsersCommonsApplication();
 		log.info(common.hello());
+		
+		User user = new User();
+		user.setName("Test user");
+		log.info(user.getName());
+		
+		Role role = new Role();
+		role.setName("Test role");
+		log.info(role.getName());
+		
 		SpringApplication.run(SpringBootServiceUsersApplication.class, args);
 		
 	}
